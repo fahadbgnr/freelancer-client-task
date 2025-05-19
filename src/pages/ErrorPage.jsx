deb594366e56ad7d4e5451c1cba6654b2f076350
fahadbgnr <aminulislamfahad1@gmail.com>
2025-05-20 00:48:15 +0600

    error page complete

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Link, useRouteError } from 'react-router';
 
 const ErrorPage = () => {
+    const error = useRouteError();
     return (
         <div>
-            ErrorPage
+             <div>
+            <div className=' py-24 text-center' >
+                {/* <Helmet>
+                    <title>Errorpage</title>
+                </Helmet> */}
+                <h1 className='mb-8 text-4xl lg:text-7xl font-semibold text-accent'>
+                    {
+                        error?.status || 404
+                    }
+                </h1>
+
+                <p className='mb-3  text-xl font-bold text-secondary md:text-2xl '>
+                    {error?.error?.massage || "Something want wrong!"}
+
+                </p>
+                <Link className='btn btn-secondary rounded-2xl' to='/'>
+                Go To Homepage
+
+                </Link>
+
+            </div>
+        </div>
         </div>
     );
 };
